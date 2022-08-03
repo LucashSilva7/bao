16de2fead443df0c38d3c4369186a0c27d7ba831
Lucas <96627066+LucashSilva7@users.noreply.github.com>
2022-08-02 23:43:55 -0300

    a

---
+++ main.js
@@ -0,0 +1,42 @@
+const textToUser = document.querySelector('#text-to-user');
+const userName = document.querySelector('#user-name');
+const buttonSubmit = document.querySelector('#button-submit');
+
+function whatName() {
+    nameDefault = userName.value;
+    if (
+        nameDefault == 'Thaíse' ||
+        nameDefault == 'Thaise' ||
+        nameDefault == 'thaíse' ||
+        nameDefault == 'thaise' ||
+        nameDefault == 'taise' 
+    ) {
+        textToUser.textContent = 'Minha namorada';
+    } else if (
+        nameDefault == 'Silas' ||
+        nameDefault == 'silas' ||
+        nameDefault == 'Wenders' ||
+        nameDefault == 'wenders'
+    ) {
+        textToUser.textContent = 'Ele é meu amigo!';
+    } else if (
+        nameDefault == 'thalyssa' ||
+        nameDefault == 'Thalyssa' ||
+        nameDefault == 'thaty' ||
+        nameDefault == 'Thaty' ||
+        nameDefault == 'taty' ||
+        nameDefault == 'Taty' ||
+        nameDefault == 'thayane' ||
+        nameDefault == 'Thayane' ||
+        nameDefault == 'thayanne' ||
+        nameDefault == 'Thayanne' ||
+        nameDefault == 'Vitória'
+    ) {
+        textToUser.textContent = 'Ela é minha amiga';
+    } else {
+        textToUser.textContent = 'Não sei quem é, Acho que não lembro :[';
+    }
+}
+
+buttonSubmit.addEventListener('click', whatName);
+userName.addEventListener('keyup', whatName);
